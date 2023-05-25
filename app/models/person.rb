3dbd43cb484d0e9c46b9184751c360e06c9a2e37
vinibispo <vini.bispo015@gmail.com>
2023-05-25 11:45:29 -0300

    feat: add validation in model person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,11 @@
 class Person < ApplicationRecord
+  validates :name, presence: true
+  validates :birth_year, presence: true
+  validates :eye_color, presence: true
+  validates :gender, presence: true
+  validates :hair_color, presence: true
+  validates :height, presence: true
+  validates :mass, presence: true
+  validates :skin_color, presence: true
+  validates :homeworld, presence: true
 end
