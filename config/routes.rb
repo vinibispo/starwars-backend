5f7c204f2c4ebde037b73bbddaa1e52752f9a885
vinibispo <vini.bispo015@gmail.com>
2023-05-25 11:21:15 -0300

    feat: add people routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :planets
   resources :films
+  resources :people
+  resources :planets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
