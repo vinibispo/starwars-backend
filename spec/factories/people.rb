65f12b34bb98c0998f63c273b6c032c46a17b9d0
vinibispo <vini.bispo015@gmail.com>
2023-05-25 11:14:34 -0300

    test: add factories for person model

---
+++ spec/factories/people.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :person do
+    name { Faker::Name.name }
+    birth_year { Faker::Number.between(from: 1900, to: 2020) }
+    eye_color { Faker::Color.color_name }
+    gender { %w[male female n/a].sample }
+    hair_color { Faker::Color.color_name }
+    height { Faker::Number.between(from: 50, to: 250) }
+    mass { Faker::Number.between(from: 50, to: 250) }
+    skin_color { Faker::Color.color_name }
+    homeworld { nil }
+  end
+end
