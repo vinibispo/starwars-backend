8535f54dffcf513265f8d4d8dafcb3a8d16863b4
vinibispo <vini.bispo015@gmail.com>
2023-05-25 07:28:40 -0300

    test: add planet factory

---
+++ spec/factories/planets.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :planet do
+    name { Faker::Space.planet }
+    diameter { Faker::Space.distance_measurement }
+    rotation_period { Faker::Space.distance_measurement }
+    orbital_period { Faker::Space.distance_measurement }
+    gravity { Faker::Space.distance_measurement }
+    population { Faker::Space.distance_measurement }
+    climate { 'arid' }
+    terrain { 'desert' }
+    surface_water { Faker::Space.distance_measurement }
+  end
+end
