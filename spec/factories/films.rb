af35c62b367193822dc3ce77ab76d185239f5d73
vinibispo <vini.bispo015@gmail.com>
2023-05-25 08:46:52 -0300

    test: add films factory

---
+++ spec/factories/films.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :film do
+    title { Faker::Movie.title }
+    episode_id { Faker::Number.between(from: 1, to: 10) }
+    opening_crawl { Faker::Lorem.paragraph }
+    director { Faker::Name.name }
+    producer { Faker::Name.name }
+    release_date { Faker::Date.between(from: '1977-01-01', to: '2020-12-31') }
+  end
+end
