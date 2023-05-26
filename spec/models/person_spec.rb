53356c5b4523085d3a7c87c174f68ae7e7f04b57
vinibispo <vini.bispo015@gmail.com>
2023-05-25 21:19:22 -0300

    test: add validation for person model

---
+++ spec/models/person_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Person, type: :model do
+  context 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:birth_year) }
+    it { should validate_presence_of(:eye_color) }
+    it { should validate_presence_of(:gender) }
+    it { should validate_presence_of(:hair_color) }
+    it { should validate_presence_of(:height) }
+    it { should validate_presence_of(:mass) }
+    it { should validate_presence_of(:skin_color) }
+    it { should validate_presence_of(:homeworld) }
+  end
+end
