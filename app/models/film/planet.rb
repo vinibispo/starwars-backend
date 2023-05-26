5e46496e22db615638fcbfa70ce889afe78dfb73
vinibispo <vini.bispo015@gmail.com>
2023-05-26 11:26:08 -0300

    fix: film_planet doesn't find planet reference

--- app/models/film/planet.rb
+++ app/models/film/planet.rb
@@ -1,5 +1,5 @@
 class Film::Planet < ApplicationRecord
   self.table_name = 'film_planet'
-  belongs_to :film
-  belongs_to :planet
+  belongs_to :film, class_name: 'Film'
+  belongs_to :planet, class_name: '::Planet'
 end
