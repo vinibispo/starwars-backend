733ae4075c72e740925c3912930643e619d5f4bc
vinibispo <vini.bispo015@gmail.com>
2023-05-30 09:49:36 -0300

    fix(cors): liberate post requests

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,7 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
              headers: :any,
-             methods: [:get],
+             methods: %i[get post],
              expose: %w[Total-Count Total-Pages]
   end
 end
